00c9e4733f63d8c2684fe0751baae7ec5633b00c
Md Kaif <md.71.kaif@gmail.com>
2025-04-27 03:21:52 +0530

    feat: Configure Next.js for standalone output and optimize package imports for Clerk

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
+  experimental: {
+    optimizePackageImports: ['@clerk/nextjs'],
+  }
 };
 
 export default nextConfig;
